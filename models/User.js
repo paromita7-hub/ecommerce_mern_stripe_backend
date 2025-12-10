aa12f7f983f2e71864f40f058e12dea85c7d19d9
Paromita Porel <paromitaporel41@gmail.com>
2025-12-10 21:02:53 +0530

    Make User pre-save hook robust: try/catch & safe next(err)

--- models/User.js
+++ models/User.js
@@ -27,16 +27,20 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-// Pre-save hook to hash password before saving
+// Pre-save hook to hash password before saving (robust try/catch)
 userSchema.pre("save", async function (next) {
-  // Only hash if the password is modified/new
-  if (!this.isModified("password")) return next();
+  try {
+    // Only hash if the password is modified/new
+    if (!this.isModified("password")) return next();
 
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
-  next();
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    return next();
+  } catch (err) {
+    // ensure next is called with error so Mongoose knows something failed
+    return typeof next === "function" ? next(err) : Promise.reject(err);
+  }
 });
-
 // Method to compare entered password with hashed one
 userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
